314f8eeb4c755780f422b7c046dd60d9c4bf570f
Pavan <k.jvspk3@gmail.com>
2026-07-22 21:53:40 +0530

    Add: string-encode-and-decode - submission-15

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-15.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        s = ''
+        for i in strs:
+            s += str(len(i))+'-'+i
+        return s
+
+
+    def decode(self, s: str) -> List[str]:
+        r, i =[], 0
+        while i < len(s):
+            j = i
+            while s[j] != '-':
+                j+=1
+            length = int(s[i:j])
+            r.append(s[j+1: j+1+length])
+            i = j+1+length
+        return r
+    
